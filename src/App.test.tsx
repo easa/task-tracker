673ca39755fe0e90b66528b309fc0b63bb836dc0
easa <eisanodehi@gmail.com>
2021-12-13 23:23:57 +0330

    update package json , add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { useSelector, useDispatch } from 'react-redux';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+// FIXME: write a valid test
+const mockDispatch = jest.fn();
+const mockedDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+}));
+
+describe('App', () => {
+  beforeEach(() => {
+    const mockState = { task: { tasks: [], currentId: '' } };
+    useSelector.mockImplementation((callback: any) => callback(mockState));
+  });
+  afterEach(() => {
+    useSelector.mockClear();
+  });
+  test('renders App component', () => {
+    // useSelector.mockImplementation((selectorFn: any) => selectorFn());
+    // useDispatch.mockReturnValue(mockedDispatch);
+    // render(<App />);
+    // expect(mockDispatch).toHaveBeenCalledWith(/* arguments your expect */);
+
+    // render(<App />);
+
+    screen.debug();
+  });
 });
